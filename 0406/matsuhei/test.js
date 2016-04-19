379df79da3d6b08966a6c63349e40c722a8fcff1
matsuhei <n232076r@mail.kyutech.jp>
2016-04-19 14:16:45 +0900

    Snake case

--- 0406/matsuhei/test.js
+++ 0406/matsuhei/test.js
@@ -1,10 +1,9 @@
-  function show_img1(){
-  document.area1.src = "preview1.png";
-  }
-  function show_img2(){
-  document.area1.src = "preview2.png";
-  }
-  function show_img3(){
-  document.area1.src = "preview3.png";
-  }
-
+   function show_img1(){
+   document.area1.src = "preview1.png";
+   }
+   function show_img2(){
+   document.area1.src = "preview2.png";
+   }
+   function show_img3(){
+   document.area1.src = "preview3.png";
+   }
