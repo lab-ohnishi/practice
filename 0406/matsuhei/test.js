e2d57b3cab71392181a32deda00ea9ce7773198c
matsuhei <n232076r@mail.kyutech.jp>
2016-04-21 16:13:49 +0900

    0406 delete

--- 0406/matsuhei/test.js
+++
@@ -1,9 +0,0 @@
-   function show_img1(){
-   document.area1.src = "preview1.png";
-   }
-   function show_img2(){
-   document.area1.src = "preview2.png";
-   }
-   function show_img3(){
-   document.area1.src = "preview3.png";
-   }
