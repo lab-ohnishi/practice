32bf2d2885b6890837337b7d2730e58da1b0d940
avenue <n232074s@mail.kyutech.jp>
2016-04-26 15:03:21 +0900

    add displayed_rest

--- 0422/avenue/Gacha1.js
+++ 0422/avenue/Gacha1.js
@@ -2,10 +2,19 @@
 
 window.addEventListener("load",function(){
 	var chara = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z'];
+	var chara_rest = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z'];
 	$('#start').click(function() {
-		var rand = chara[ Math.floor( Math.random() * chara.length)];		
+		
+		 
+		var x; 
+		x = Math.floor( Math.random() * chara.length);
+		var rand = chara[x];		
 		document.getElementById( "result" ).textContent = rand ;
+
 		$('#histories').prepend('<li>'+rand+'</li>');
+		chara_rest.splice(x,1);
+    document.getElementById( "rest" ).textContent = chara_rest ;
+
 
 	});
 });
