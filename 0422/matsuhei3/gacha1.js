1ad6de13cdedcf8b2ac57797150b0d6633b4d458
matsuhei <n232076r@mail.kyutech.jp>
2016-04-27 15:04:04 +0900

    functionでの分割

--- 0422/matsuhei3/gacha1.js
+++ 0422/matsuhei3/gacha1.js
@@ -1,26 +1,38 @@
 window.addEventListener("load",function(){
   var alpha = [];
-  var until = [];
+  var rest = [];
   var result = [];
+  var rand_num;
 
-  $('#start').click(function(){
+  function initialize(alpha,rest){
     if(alpha[0] != "a"){
       for(var i=97;i<=122;i++){
         alpha.push(String.fromCharCode(i));
-        until.push(String.fromCharCode(i));
+        rest.push(String.fromCharCode(i));
       }
     }
+  }
+
+  function rest_delete(alpha,rest,rand_num){
     for(var j=0;j<26;j++){
-      if(until[j] == alpha[rand_num]){
-        until[j] = "-";
+      if(rest[j] == alpha[rand_num]){
+        rest[j] = "-";
         break;
       }
     }
-    result.unshift(alpha[rand_num]);
+  }
 
+  function Display(alpha,rest,result,rand_num){
     document.getElementById('result').innerHTML = alpha[rand_num];
-    document.getElementById('rest').innerHTML = until;
+    document.getElementById('rest').innerHTML = rest;
     document.getElementById('histories').innerHTML = result;
+  }
 
+  $('#start').click(function(){
+    initialize(alpha,rest);
+    rand_num = Math.floor(Math.random()*26);
+    rest_delete(alpha,rest,rand_num);
+    result.unshift(alpha[rand_num]);
+    Display(alpha,rest,result,rand_num);
   });
 });
