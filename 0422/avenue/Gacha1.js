a335b1539bcb8d8c7b470d8968ac3be786b1889a
avenue <n232074s@mail.kyutech.jp>
2016-04-28 21:10:25 +0900

    divided_function

--- 0422/avenue/Gacha1.js
+++ 0422/avenue/Gacha1.js
@@ -1,29 +1,33 @@
-
-
 window.addEventListener("load",function(){
+	var preset = new Array();
 	var chara_rest = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z'];
+	var selected = '';
+	var random_num;
 
-	var array = new Array();
-	var str = '';
 	$('#start').click(function() {
-
-
-
-		for(var i = 0; i < 26; i++){
-			array.push(String.fromCharCode('a'.charCodeAt() + i))	
-		}
-
-		rand = [Math.floor( Math.random() * 26)];
-		str = array[rand];
-
-		document.getElementById( "result" ).textContent = str ;
-
-		$('#histories').prepend('<li>'+str+'</li>');
-
-		delete chara_rest[rand];
-		document.getElementById( "rest" ).textContent = chara_rest ;
-
+		preset_num(preset);
+		selected_num(random_num);
+		histories(selected);
+		rest(random_num,chara_rest);
 	});
+
+	function preset_num(){
+		for(var i = 0; i < 26; i++){
+			preset.push(String.fromCharCode('a'.charCodeAt() + i));	
+		}
+	}
+	function selected_num(){
+		random_num = Math.floor( Math.random() * 26);
+		selected = preset[random_num];
+		document.getElementById( "result" ).textContent = selected;
+	}
+	function histories(){
+		$('#histories').prepend('<li>'+selected+'</li>');
+	}
+	function rest(){
+		delete chara_rest[random_num];
+		document.getElementById( "rest" ).textContent = chara_rest ;
+	}
 });
 
 
