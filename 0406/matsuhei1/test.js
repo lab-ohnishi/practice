7d6c031a94f4ac62d3458425cf40b7545909d44d
matsuhei <n232076r@mail.kyutech.jp>
2016-04-30 17:35:25 +0900

    css:button1の変更,関数のまとめ,改行の修正

--- 0406/matsuhei1/test.js
+++ 0406/matsuhei1/test.js
@@ -1,10 +1,10 @@
-  function show_img1(){
-  document.area1.src = "preview1.png";
-  }
-  function show_img2(){
-  document.area1.src = "preview2.png";
-  }
-  function show_img3(){
-  document.area1.src = "preview3.png";
-  }
+window.addEventListener("load",function(){
 
+  function update(button){
+    var up_img = button.attr('img_data');
+    document.area1.src = up_img;
+  }
+  $(".chenge_Img_button").click(function(){
+    update($(this));
+  });
+});
